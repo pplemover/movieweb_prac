db036692d1cdc528385e884ab76a6733534fe09c
paulimbest <paulimbest@naver.com>
2023-04-14 15:41:05 +0900

    3:40

--- pjt_6/accounts/urls.py
+++ pjt_6/accounts/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('signout/', views.signout, name='signout'),
-    path('update/', views.update, name='update'),
+    path('delete/', views.delete, name='delete'),
+    path('update/', views.update, name="update"),
     path('password/', views.change_password, name='change_password'),
-    # 단일 회원 프로필 조회
+    
     path('profile/<str:username>/', views.profile, name='profile'),
+    path('<int:user_pk>/follow/', views.follow, name='follow'),
 ]
